18367487bc62f7950e953bf8c9f09712890feb53
ljh5945 <ljh594545@gmail.com>
2024-02-26 22:17:27 +0900

    [Fix]_01/Inventory.cs_Item
    1. 인벤토리.cs 파일 수정
    - 아이템 없으면 오류나는 것들 제외 모두 추가

--- Assets/Scripts/Item/Inventory.cs
+++ Assets/Scripts/Item/Inventory.cs
@@ -1,6 +1,8 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using static ItemSlot; // 삭제 예정
 
 public class ItemSlot
 {
@@ -10,6 +12,7 @@ public class ItemSlot
     public class ItemData  // 삭제 예정
     {
         internal readonly Sprite icon; // 삭제 예정
+        internal bool canStack;  // 삭제 예정
     }
 }
 
@@ -54,7 +57,14 @@ public class Inventory : MonoBehaviour
         inventoryWindow.SetActive(false);
         slots = new ItemSlot[uiSlots.Length];
 
-        
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i] = new ItemSlot();
+            uiSlots[i].index = i;
+            uiSlots[i].Clear();
+        }
+
+        ClearSeletecItemWindow();
     }
 
     private class PlayerController  // 삭제 예정
@@ -63,5 +73,112 @@ public class Inventory : MonoBehaviour
 
     private class PlayerConditions  // 삭제 예정
     {
+    }
+
+    public void Toggle()
+    {
+        if (inventoryWindow.activeInHierarchy)
+        {
+            inventoryWindow.SetActive(false);
+        }
+        else
+        {
+            inventoryWindow.SetActive(true);
+        }
+    }
+
+    public bool IsOpen()
+    {
+        return inventoryWindow.activeInHierarchy;
+    }
+
+    public void AddItem(ItemData item)
+    {
+        if (item.canStack)
+        {
+            ItemSlot slotToStackTo = GetItemStack(item);
+            if (slotToStackTo != null)
+            {
+                slotToStackTo.quantity++;
+                UpdateUI();
+                return;
+            }
+        }
+
+        void ThrowItem(ItemData item)
+        {
+
+        }
+
+        void UpdateUI()
+        {
+
+        }
+
+        ItemSlot GetItemStack(ItemData item)
+        {
+            return null;
+        }
+
+        ItemSlot GetEmptySlot()
+        {
+            return null;
+        }
+    }
+
+    public void SelectItem(int index)
+    {
+
+    }
+
+    private void ClearSeletecItemWindow()
+    {
+
+    }
+
+    public void OnUseButton()
+    {
+
+    }
+
+    public void OnEquipButton()
+    {
+
+    }
+
+    void UnEquip(int index)
+    {
+
+    }
+
+    public void OnUnEquipButton()
+    {
+
+    }
+
+    public void OnDropButton()
+    {
+        ThrowItem(selectedItem.item);
+        RemoveSelectedItem();
+    }
+
+    private void ThrowItem(ItemData item)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void RemoveSelectedItem()
+    {
+
+    }
+
+    public void RemoveItem(ItemData item)
+    {
+
+    }
+
+    public bool HasItems(ItemData item, int quantity)
+    {
+        return false;
     }
 }
