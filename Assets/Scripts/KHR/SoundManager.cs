e3ebd6d3023d10b00278fca24ae429e0f185c035
baekwinter <kksbiy0012@gmail.com>
2024-02-26 21:40:26 +0900

    [Add]_03/SDM_Data
    사운드매니저 기본틀

--- Assets/Scripts/KHR/SoundManager.cs
+++ Assets/Scripts/KHR/SoundManager.cs
@@ -2,8 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SoundManager : MonoBehaviour
 {
-   // 배열 
+    public AudioClip[] eatingSounds;
+    public AudioClip[] walkingSounds;
+    public AudioClip[] choppingTreeSounds;
+    public AudioClip[] waterSounds;
+    //public AudioClip[] rainSounds;
+    public AudioClip[] fishingSounds;
+    public AudioClip[] gatheringSounds;
+    public AudioClip[] gunshotSounds;
+    public AudioClip[] zombieSounds;
+    public AudioClip[] hittingZombieSounds;
+    public AudioClip[] zombieAttackingSounds;
+    public AudioClip[] playerTakingDamageSounds;
+    public AudioClip[] boarSounds;
+    public AudioClip[] hittingBoarSounds;
+    public AudioClip[] boarWalkingSounds;
+    public AudioClip[] rabbitRunningSounds;
+    public AudioClip[] hittingRabbitSounds;
+    public AudioClip[] rabbitFleeingSounds;
+    public AudioClip[] walkingOnGrassSounds;
+    public AudioClip[] walkingOnSandSounds;
+    public AudioClip[] walkingOnRockSounds;
+    public AudioClip[] walkingOnTreeSounds;
+    public AudioClip[] burningSounds;
+    public AudioClip[] hittingFlintSounds;
+    public AudioClip[] swordClashSounds;
+    public AudioClip[] shootingSounds;
 
+    // 사운드 매니저 스크립트의 나머지 부분은 여기에 작성
+    // 예를 들어, 특정 상황에서 해당 소리를 재생하는 함수들을 추가
 }
+
