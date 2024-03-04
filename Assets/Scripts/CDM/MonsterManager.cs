217c2debab5d25538a2f7ceaad2fe5120b9e32dd
chamin47 <152050575+chamin47@users.noreply.github.com>
2024-03-04 10:32:41 +0900

    [Addfix]_MonsterManager.cs 수정
    몬스터 씬에 스폰 포인트 빈 오브젝트로 찍는중..

--- Assets/Scripts/CDM/MonsterManager.cs
+++ Assets/Scripts/CDM/MonsterManager.cs
@@ -3,59 +3,14 @@ using UnityEngine;
 
 public class MonsterManager : MonoBehaviour
 {
-	public GameObject rabbitPrefab;
-	public GameObject bearPrefab;
+	public Transform[] points;
 
-	public int maxRabbits = 300; // 원하는 토끼의 최대 수
-	public int maxBears = 100; // 원하는 베어의 최대 수
-
-	public float rabbitSpawnInterval = 3f; // 토끼 소환 간격
-	public float bearSpawnInterval = 5f; // 베어 소환 간격
-
-	private int currentRabbitCount = 0;
-	private int currentBearCount = 0;
-
-	public Vector3 minimumBoundary; // 배치할 최소 경계
-	public Vector3 maximumBoundary; // 배치할 최대 경계
-
-	void Start()
+	private void Start()
 	{
-		StartCoroutine(SpawnRabbitsCoroutine());
-		StartCoroutine(SpawnBearsCoroutine());
-	}
+		// SpawnPointGroup 게임 오브젝트의 Transform 컴포넌트 추출
+		Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
 
-	IEnumerator SpawnRabbitsCoroutine()
-	{
-		while (currentRabbitCount < maxRabbits)
-		{
-			SpawnAnimal(rabbitPrefab);
-			yield return new WaitForSeconds(rabbitSpawnInterval);
-		}
-	}
-
-	IEnumerator SpawnBearsCoroutine()
-	{
-		while (currentBearCount < maxBears)
-		{
-			SpawnAnimal(bearPrefab);
-			yield return new WaitForSeconds(bearSpawnInterval);
-		}
-	}
-
-	private void SpawnAnimal(GameObject animalPrefab)
-	{
-		Vector3 randomPosition = GetRandomPosition();
-		Instantiate(animalPrefab, randomPosition, Quaternion.identity);
-		if (animalPrefab == rabbitPrefab) currentRabbitCount++;
-		else if (animalPrefab == bearPrefab) currentBearCount++;
-	}
-
-	// 맵의 경계 내에서 랜덤 위치를 반환합니다.
-	private Vector3 GetRandomPosition()
-	{
-		float x = Random.Range(minimumBoundary.x, maximumBoundary.x);
-		float y = Random.Range(minimumBoundary.y, maximumBoundary.y);
-		float z = Random.Range(minimumBoundary.z, maximumBoundary.z);
-		return new Vector3(x, y, z);
+		// SpawnPointGroup 하위에 있는 모든 차일드 게임오브젝트의 Transform 컴포넌트 추출
+		points = spawnPointGroup?.GetComponentsInChildren<Transform>();
 	}
 }
